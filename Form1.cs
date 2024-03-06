e771230ce82414125233b5cc0b8ac676f08143c9
Alumno <Alumno@L13-2543299>
2024-03-06 11:30:22 -0600

    se agrego la actualizacion de la variable resultado en el label

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,13 @@ namespace Formcomplejso
         private Complejo complejo2;
         private Complejo complejo3;
 
+        public bool btnConjugar_Clic { get; private set; }
 
         public Form1()
         {
             InitializeComponent();
         }
+
 
         private void btnSumar_Click(object sender, EventArgs e)
         {
@@ -34,7 +36,15 @@ namespace Formcomplejso
 
         private void btnMultiplicar_Click(object sender, EventArgs e)
         {
-
+            complejo1 = Complejo.Leer(txtbcomplejo1.Text);
+            complejo2 = Complejo.Leer(txtbcomplejo2.Text);
+            complejo3 = complejo1 * complejo2;
+            lbResultComplejo.Text = complejo3.ToString();
+        }
+        private void btnConjugar_Click(object sender, EventArgs e)
+        {
+             complejo3 = !complejo3;
+             lbResultComplejo.Text = complejo3.ToString();
         }
 
         private void btnSuma_MouseHover(object sender, EventArgs e)
@@ -52,6 +62,10 @@ namespace Formcomplejso
             lboperador.Text = "*";
         }
 
-
+        private void btnConjugar_MouseHover(object sender, EventArgs e)
+        {
+            lboperador.Text = "!";
+        }
     }
 }
+
